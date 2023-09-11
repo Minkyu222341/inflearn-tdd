5acfccfb3b5f029abd0af0404dddca542459ed04
Minkyu222341 <qkralsrb2040@naver.com>
2023-09-11 11:20:41 +0900

    feat : 섹션 2 단위 테스트
    * 단위 테스트 - 수동 테스트, 자동화 테스트
    * Junit5 , AssertJ
    * 해피케이스 , 예외케이스
    * 경계값 테스트
    * 테스트하기 쉬운/어려운 영역을 나누어서 효과적으로 설계 (순수함수)

--- src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
+++ src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
@@ -1,4 +1,4 @@
-package sample.cafekiosk;
+package sample.cafekiosk.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +11,8 @@ public class CafekioskApplication {
 	}
 
 }
+
+
+
+
+
